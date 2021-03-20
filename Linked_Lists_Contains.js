4c7636515f53e75bc5c27346b6e0e957200535ed
James Haltiwanger <jhalt@bellsouth.net>
2021-03-19 21:12:37 -0400

    initial Linked List Contains

--- Linked_Lists_Contains.js
+++ Linked_Lists_Contains.js
@@ -7,8 +7,9 @@ class Node {
 }
 
 // 1.) Add Front. Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
-//2.) Remove Front. Write a method to remove the head node and return the new list head node. If the list is empty, return null.
-//3.) Front. Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+// 2.) Remove Front. Write a method to remove the head node and return the new list head node. If the list is empty, return null.
+// 3.) Front. Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+// 4.) Contains. Add a method contains(value) to your SLL class, which is given a value as a parameter.  Return a boolean (true/false); true, if the list possesses a node that contains the provided value.
 class SLL {
     constructor() {
         this.head = null;
@@ -33,6 +34,17 @@ class SLL {
         }
         return null;
     }
+
+    contains(value) {
+        var runner = this.head;
+        while(runner) {
+            if(runner.value === value) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
 }
 
 var ssList = new SLL(); //creates new instance of an SSL object
@@ -43,4 +55,4 @@ ssList.addFront(second); // moves 1 to second node, and makes head node equal to
 console.log(ssList.head); //result is "Node {value: 2, next: Node}"
 ssList.removeFront(); //removes current head node, leaving 1 as new head node. 
 console.log(ssList.front()); //result is "1" because it returns the value, instead of the whole node. 
-
+console.log(ssList.contains(1)); //result is "true"
